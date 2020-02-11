2926224ac7ab933b3a7dc6156696bd3572e1f541
Pietro <pietro.schiavinato@unesp.br>
2020-02-11 00:41:46 -0300

    Add a constant file

---
+++ constants.js
@@ -0,0 +1,14 @@
+const resultPath = 'results';
+const responsePath = 'responses';
+const examplesPath = 'examples';
+const examplesTestPath = 'examples/tests';
+const responseTestPath = 'responses/tests';
+
+const detectionTypes = {
+    FACE: 'face',
+    OBJECT: 'object',
+    TEXT: 'text',
+    PROPERTIES: 'properties',
+}
+
+module.exports = {resultPath, responsePath, examplesPath, examplesTestPath, responseTestPath, detectionTypes}
